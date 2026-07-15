44b3b1deedfa9deda3e864827388a8a5245b677d
taniya221 <taniya.jaison007@gmail.com>
2026-07-15 09:53:53 +0530

    feature/viewTeam added

--- src/components/ViewTeam.jsx
+++ src/components/ViewTeam.jsx
@@ -1,85 +1,73 @@
-import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
+import axios from 'axios'
+
 const ViewTeam = () => {
-     const [data, changeData] = useState([])
+    const [data, changeData] = useState([])
 
     const fetchData = () => {
-        axios.get("http://localhost:3000/view-hackathon").then(
-
+        axios.post("http://localhost:3000/view-team").then(
             (response) => {
                 changeData(response.data)
             }
-
         ).catch()
     }
 
-    useEffect (
+    useEffect(
         () => {
             fetchData()
         }, []
     )
-  return (
-  <div>
-           
+
+    return (
+        
 
             <div className="container">
-                <div className="row">
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+             
+    <div className="row">
+        <div className="col-12">
 
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">teamID</th>
-                                    <th scope="col">teamName</th>
-                                    <th scope="col">teamLeaderName</th>
-                                    <th scope="col">leaderEmail</th>
-                                    <th scope="col">leaderPhone</th>
-                                    <th scope="col">collegeName</th>
-                                    <th scope="col">numberofMembers</th>
-                                    <th scope="col">projectTitle</th>
-                                    <th scope="col">problemStatementTrack</th>
-                                    <th scope="col">technologyStack</th>
-                                    <th scope="col">mentorName</th>
-                                    <th scope="col">registrationDate</th>
-                                    <th scope="col">tableStationNumber</th>
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Team ID</th>
+                        <th>Team Name</th>
+                        <th>Team Leader Name</th>
+                        <th>Leader Email</th>
+                        <th>College Name</th>
+                        <th>No. of Members</th>
+                        <th>Project Title</th>
+                        <th>Problem Statement</th>
+                        <th>Technology Stack</th>
+                        <th>Mentor Name</th>
+                        <th>Registration Date</th>
+                        <th>Table</th>
+                    </tr>
+                </thead>
 
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    data.map(
-                                        (value, index) => {
-                                            return (
-                                                <tr>
-                                                    <td>{value.teamID}</td>
-                                                    <td>{value.teamName}</td>
-                                                    <td>{value.teamLeaderName}</td>
-                                                    <td>{value.leaderEmail}</td>
-                                                    <td>{value.leaderPhone}</td>
-                                                    <td>{value.collegeName}</td>
-                                                    <td>{value.numberofMembers}</td>
-                                                    <td>{value.projectTitle}</td>
-                                                    <td>{value.problemStatementTrack}</td>
-                                                    <td>{value.technologyStack}</td>
-                                                    <td>{value.mentorName}</td>
-                                                    <td>{value.registrationDate}</td>
-                                                    <td>{value.tableStationNumber}</td>
-                                                   
-                                                </tr>
-                                            )
-                                        }
-                                    )
-                                }
-
-                            </tbody>
-                        </table>
-
-                    </div>
-                </div>
-            </div>
+                <tbody>
+                    {data.map((value, index) => (
+                        <tr key={index}>
+                            <td>{value.teamId}</td>
+                            <td>{value.teamName}</td>
+                            <td>{value.teamLname}</td>
+                            <td>{value.leaderEmail}</td>
+                            <td>{value.collegeName}</td>
+                            <td>{value.noMembers}</td>
+                            <td>{value.projectTitle}</td>
+                            <td>{value.problemStat}</td>
+                            <td>{value.techStack}</td>
+                            <td>{value.mentorName}</td>
+                            <td>{value.regDate}</td>
+                            <td>{value.table}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
 
         </div>
+    </div>
+</div>
     )
 }
 
