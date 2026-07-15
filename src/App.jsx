ccc49d308e83fecb3681a27d3882ae5db7d1dd19
taniya221 <taniya.jaison007@gmail.com>
2026-07-15 08:49:22 +0530

    feature updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import AddTeam from './components/AddTeam'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <AddTeam />
     </>
   )
 }
